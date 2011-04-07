74d314e243a25982aa7e1c3370eb8f59b635cfa8
LIS467 <lis467@soundwave.(none)>
2011-04-07 19:41:10 -0400

    Thursday, 4/7/2011 - 7:39pm Cleanup and commenting changes to CSS, name added to google map.

--- footer.php
+++ footer.php
@@ -1,10 +1,8 @@
 </div> <!-- end content -->
 </div> <!-- end non-footer -->
 <footer>
-<p>T. B. Scott Free Library<br />
-106 West First Street<br />
-Merrill, WI 54452</p>
-<p>Telephone: (715) 536-7191 | Fax: (715) 536-1705</p>
+<p>Address: 106 West First Street, Merrill, WI 54452</p>
+    <p>Telephone: (715) 536-7191 | Fax: (715) 536-1705&nbsp;&nbsp;</p>
 </footer>
 
 
