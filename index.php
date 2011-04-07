d445d5f98472cd958643b06ea2471a79b6ec1f1d
LIS467 <lis467@soundwave.(none)>
2011-04-07 17:32:33 -0400

    Thursday, 4/7/2011 - 5:31pm Reduced font size to 14px, moved form above map.

--- index.php
+++ index.php
@@ -5,10 +5,10 @@ include 'header.php'; ?>
 <h2>Library Hours</h2>
 <h3>General Hours:</h3>
 <ul>
-<li><em>Mon - Thurs</em> : 9:30 am - 8 pm</li>
-<li><em>Friday</em> : 9:30 am - 6 pm</li>
-<li><em>Saturday</em> : 9:30 am - 4 pm</li>
-<li><em>Sunday</em> : Noon - 4 pm</li>
+  <li><em>Mon - Thurs:</em> 9:30 am - 8 pm</li>
+  <li><em>Friday:</em> 9:30 am - 6 pm</li>
+  <li><em>Saturday:</em> 9:30 am - 4 pm</li>
+  <li><em>Sunday:</em> Noon - 4 pm</li>
 </ul>
 
 <h3>T. B. Scott will be closed:</h3>
@@ -18,10 +18,10 @@ include 'header.php'; ?>
 <h3>Summer Hours:</h3>
 <p>Summer hours begin May 27, 2011.</p>
 <ul>
-<li><em>Mon - Thurs</em> : 9:30 am - 8 pm</li>
-<li><em>Friday</em> : 9:30 am - 5 pm</li>
-<li><em>Saturday</em> : 9:30 am - 1 pm</li>
-<li><em>Sunday</em> : Closed</li>
+  <li><em>Mon - Thurs:</em> 9:30 am - 8 pm</li>
+  <li><em>Friday:</em> 9:30 am - 5 pm</li>
+  <li><em>Saturday:</em> 9:30 am - 1 pm</li>
+  <li><em>Sunday:</em> Closed</li>
 </ul>
 </section> <!-- end hours -->
 
@@ -41,14 +41,14 @@ include 'header.php'; ?>
 
 <section id="directions">
 <h2>Map and Directions</h2>
-<iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=106+West+First+Street+Merrill,+WI+54452&amp;aq=&amp;sll=37.0625,-95.677068&amp;sspn=42.987658,96.152344&amp;ie=UTF8&amp;hq=&amp;hnear=106+1st+St,+Merrill,+Wisconsin+54452&amp;ll=45.184033,-89.701653&amp;spn=0.025711,0.036478&amp;z=14&amp;iwloc=A&amp;output=embed"></iframe><p>View <a href="http://maps.google.com/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=106+West+First+Street+Merrill,+WI+54452&amp;aq=&amp;sll=37.0625,-95.677068&amp;sspn=42.987658,96.152344&amp;ie=UTF8&amp;hq=&amp;hnear=106+1st+St,+Merrill,+Wisconsin+54452&amp;ll=45.184033,-89.701653&amp;spn=0.025711,0.036478&amp;z=14&amp;iwloc=A" style="color:#0000FF;text-align:left">T B Scott Free Library</a> in a larger map</p>
-<p>Get directions from wherever you are!</p>
+<p><strong>Get directions from wherever you are!</strong></p>
 <form action="http://maps.google.com/maps"  method="get">
 Where are you right now?:
 <input type="text" name="saddr" />
 <input type="hidden" name="daddr" value="106 W. First St., Merrill, WI 54452" />
 <input type="submit" value="Go!" />
 </form>
+<iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=106+West+First+Street+Merrill,+WI+54452&amp;aq=&amp;sll=37.0625,-95.677068&amp;sspn=42.987658,96.152344&amp;ie=UTF8&amp;hq=&amp;hnear=106+1st+St,+Merrill,+Wisconsin+54452&amp;ll=45.184033,-89.701653&amp;spn=0.025711,0.036478&amp;z=14&amp;iwloc=A&amp;output=embed"></iframe><p>View <a href="http://maps.google.com/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=106+West+First+Street+Merrill,+WI+54452&amp;aq=&amp;sll=37.0625,-95.677068&amp;sspn=42.987658,96.152344&amp;ie=UTF8&amp;hq=&amp;hnear=106+1st+St,+Merrill,+Wisconsin+54452&amp;ll=45.184033,-89.701653&amp;spn=0.025711,0.036478&amp;z=14&amp;iwloc=A" style="color:#0000FF;text-align:left">T B Scott Free Library</a> in a larger map</p>
 </section> <!-- end directions -->
 
   <?php include 'footer.php'; ?>
