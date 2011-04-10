916a36270c0f587ce6859c440a0ed1c31ca77b20
William Mayo <mayow@simmons.edu>
2011-04-10 18:55:59 -0400

    Sunday, 4/10/2011 - 6:53pm Added template file.

---
+++ template.php
@@ -0,0 +1,9 @@
+<?php 
+$title = 'TITLE_OF_PAGE_GOES_HERE';
+$section = 'SECTION_OF_TOPLEVEL_NAVIGATION_GOES_HERE';
+$thispage = 'template.php'; <!-- This should be the current page url! -->
+include 'header.php'; ?>
+
+<!-- CONTENT GOES HERE! -->
+
+<?php include 'footer.php'; ?>
